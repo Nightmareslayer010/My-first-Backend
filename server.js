d871f764743134a9fe57be69307d3b535e493434
Giriraj <girirajvyas010@gmail.com>
2026-05-21 20:18:37 +0530

    implemented cors

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 //instead of import we use require in node
 const express = require("express");
+const cors = require("cors");
+
 const app = express(); // execute express and save it in app variable
 
 //assign port for communication
 const PORT = 5000;
 
+//use cors (middleware)
+app.use(cors());
+
 //req and resp
 
 app.get("/api/data", (req, res) => {
   const myProject = [
-    { id: 1, name: "todolist", status: "completed" },
-    { id: 2, name: "weatherApp", status: "completed" },
-    { id: 3, name: "backendproject", status: "active" },
+    { id: 1, text: "task 1", completed: true },
+    { id: 2, text: "task 1", completed: true },
+    { id: 3, text: "task 1", completed: true },
   ];
 
   //response
